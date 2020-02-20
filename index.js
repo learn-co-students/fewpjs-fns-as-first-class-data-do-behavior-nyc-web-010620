a155a7244aac168869bf0827143764ac51b71095
adam-xiao <adam.x.xiao@gmail.com>
2020-02-20 10:45:48 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let intTime = parseInt(time.slice(0, -3))
+
+  if (intTime >= 0 && intTime <= 12){
+    return "Good Morning"
+  } else if (intTime >= 12 && intTime <= 17){
+    return "Good Afternoon"
+  } else{
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str){
+  document.getElementById("greeting").innerText = str
+}
